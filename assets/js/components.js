6f8d58da738ad368ef50bc0b322551a75e7dbbc9
QRxFace <140483189+QRxFace@users.noreply.github.com>
2025-11-25 20:32:00 +0700

    new

---
+++ assets/js/components.js
@@ -0,0 +1,18 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const includes = document.querySelectorAll("[data-include]");
+//     includes.forEach(async el => {
+//         const file = el.getAttribute("data-include");
+//         const res = await fetch(file);
+//         el.innerHTML = await res.text();
+//     });
+// });
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Components JS loaded"); // Debug
+    const includes = document.querySelectorAll("[data-include]");
+    includes.forEach(async el => {
+        const file = el.getAttribute("data-include");
+        const res = await fetch(file);
+        el.innerHTML = await res.text();
+    });
+});
